04911a1ebec490b5b0046212498fcc9f257f6945
Deep-Kacha <deepnkacha2256@gmail.com>
2026-01-22 18:03:36 +0530

    add Header, Footer and Back To Top

--- script.js
+++ script.js
@@ -161,5 +161,24 @@ function formatDate(dateString) {
   });
 }
 
+// Back to Top Button
+const backToTopBtn = document.getElementById("backToTop");
+backToTopBtn.innerHTML = '<i class="fas fa-chevron-up"></i>';
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
 // searchInput.value = "Deep-Kacha";
 // searchUser();
